22f1c770ff808fa43880491cbb1f1c0c81248d58
Ahmet Hasırcıoğlu <ahasircioglu@gmail.com>
2020-09-30 12:51:19 +0300

    Add files via upload

---
+++ dengage.init.js
@@ -0,0 +1,12 @@
+dengage('initialize');
+
+
+function getCookie(name) {
+    function escape(s) { return s.replace(/([.*+?\^$(){}|\[\]\/\\])/g, '\\$1'); }
+    var match = document.cookie.match(RegExp('(?:^|;\\s*)' + escape(name) + '=([^;]*)'));
+    return match ? match[1] : null;
+}
+
+var userId = getCookie('eVar16');
+
+dengage('setContactKey', userId);
